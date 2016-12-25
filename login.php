d7f00492ff7dd36daf7b667daa6bf40ed21043c7
Jemar Jude Maranga <Jemar Jude Maranga>
2016-12-25 10:12:04 +0800

    files added

--- login.php
+++ login.php
@@ -1,7 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>Login</title>
+		<link type="text/css" rel="stylesheet" href="includes/Bootstrap/css/bootstrap.min.css" />
+		<link type="text/css" rel="stylesheet" href="includes/Bootstrap/css/bootstrap-theme.min.css" />
+		<link type="text/css" rel="stylesheet" href="css/style.css" />
+		<link type="text/css" rel="stylesheet" href="css/login_style.css" />
 	</head>
 	<body>
+		<?php include('header.php'); ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-xs-12">
+					<h1 class="login-heading">Login Form</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-xs-12">
+				</div>
+				<div class="col-md-6 col-xs-12">
+				</div>
+			</div>
+		</div>	
+		<script type="text/javascript" href="includes/JQuery/jquery-3.1.0.min.js"></script>
 	</body>
 </html>
