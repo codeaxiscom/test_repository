157fbee5d98318388a5332a61f0699dd0a77bbdf
Jemar Jude Maranga <Jemar Jude Maranga>
2016-12-25 10:01:38 +0800

    header added

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h1 class="site-title">Huling El Bimbo</h1>
+			</div>
+			<div class="col-md-6">
+
+			</div>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+
+<head>
+	<title>Main Page</title>
+	<link href="includes/Bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+	<link href="css/style.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+	<?php include('header.php'); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+			</div>
+			<div class="col-md-6">
+			</div>
+		</div>
+	</div>
+	<footer>
+
+	</footer>
+</body>
