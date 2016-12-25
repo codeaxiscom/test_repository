c9f356ac2b1a2518cb95ece13c5af9b59ea09f29
Jemar Jude Maranga <Jemar Jude Maranga>
2016-12-25 11:18:42 +0800

    header done

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <header>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6 header-main-container">
+			<div class="col-md-6 col-sm-12 header-main-container">
 				<h1 class="site-title"><i class="fa fa-ravelry"></i>&nbsp;&nbsp;Huling El Bimbo</h1>
 			</div>
-			<div class="col-md-6 header-main-container">
-				<p>Contact Us (+63) 933-515-5741</p>
+			<div class="col-md-6 col-sm-12 header-main-container">
+				<p>Contact Us (+63) 933-515-5741
+				<button class="btn">My Account</button></p>
 			</div>
 		</div>
 	</div>
