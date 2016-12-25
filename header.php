689732de1770bdccff214ba49d7fbbeaa74e85f0
Jemar Jude Maranga <Jemar Jude Maranga>
2016-12-25 10:13:01 +0800

    updated stuff

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
-				<h1 class="site-title">Huling El Bimbo</h1>
+				<h1 class="site-title"><i class="fa fa-ravelry"></i>Huling El Bimbo</h1>
 			</div>
 			<div class="col-md-6">
 
